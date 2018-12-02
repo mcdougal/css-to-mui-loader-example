45c114d14ffbb6023185ea928afee6b39e08214f
Cedric <mcdougal.ce@gmail.com>
2018-12-02 15:02:57 -0500

    Make css-to-mui-loader example

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import React, { Component } from "react";
+import styles from "./App.css";
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={classes.body}>
+        <Button className={classes.button}>Click Me</Button>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
